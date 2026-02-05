0edb282eb9d803dadb6391a0474e9031f04b3619
Tahira <officerpreventive@gmail.com>
2026-02-06 02:08:59 +0500

    Refactor import statements and clean up code

--- Fem hack.py
+++ Fem hack.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
@@ -387,4 +387,5 @@ if file:
 
 else:
     st.warning("Please upload the dataset to continue.")
+
 
